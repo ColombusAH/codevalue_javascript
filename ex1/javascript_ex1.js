dca04030d47da6f6dae7c857cbf857e9b15f8f76
Avner <Hattabavner@gmail.com>
2019-06-28 10:02:03 +0300

    ex1 done

---
+++ ex1/javascript_ex1.js
@@ -0,0 +1,13 @@
+const fizz_buzz = function(iteration) {
+  for (let i = 0; i < iteration; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    }
+  }
+};
+
+fizz_buzz(100);
